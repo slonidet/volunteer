2351298a773c92ff2810e4c67749478f53543113
Alex Uralov <a.uralov@wis.eu.com>
2016-12-20 09:23:57 +0300

    enable pagination

--- src/volunteer/settings.py
+++ src/volunteer/settings.py
@@ -155,13 +155,20 @@ MEDIA_ROOT = os.path.join(
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 100,
+
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.OrderingFilter',
