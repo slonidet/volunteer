d1b92892a1de40ba8a2905e864439a0619a289a1
Alex Uralov <a.uralov@wis.eu.com>
2016-12-23 16:07:54 +0300

    change permission for root api

--- src/volunteer/urls_view.py
+++ src/volunteer/urls_view.py
@@ -1,9 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 
 @api_view(['GET'])
+@permission_classes((permissions.IsAdminUser,))
 def api_root(request, format=None):
     params = {'request': request, 'format': format}
 
