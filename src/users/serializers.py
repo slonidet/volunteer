e0bfcf62f7f4a0ae5c2c9ba77305b99db7dd507d
Alex Uralov <a.uralov@wis.eu.com>
2017-01-24 11:16:25 +0300

    added user password validation

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import Group
-
-from users.models import User, Story, ProfileComment
+from django.contrib.auth.password_validation import (
+    validate_password as validate
+)
 from django.contrib.auth.hashers import make_password
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
+from users.models import User, Story, ProfileComment
 from users.models import Profile, ProfileAttachment
 
 
@@ -63,6 +65,11 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True, 'required': False}
         }
+
+    def validate_password(self, password):
+        validate(password)
+
+        return password
 
     def create(self, validated_data):
         if 'password' not in validated_data:
