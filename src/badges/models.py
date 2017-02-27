28f303b013e0b368969ded6ea61d949dec95483f
Alex Uralov <a.uralov@wis.eu.com>
2017-02-27 10:34:44 +0300

    created Badge model

--- src/badges/models.py
+++ src/badges/models.py
@@ -7,12 +7,18 @@ from users.models import User
 
 
 class Badge(models.Model):
+    TYPE_CHOICES = (
+        ('event', _('Мероприятие')),
+        ('interview', _('Интервью')),
+        ('test', _('Тест')),
+        ('profile_comment', _('Комментарий к профилю')),
+        ('user_story_comment', _('Комментарий к волонтёрской истории')),
+    )
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='badges',
         verbose_name=_('Пользователь')
     )
-    type = models.CharField(_('тип'), max_length=150)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    type = models.CharField(_('тип'), max_length=32, choices=TYPE_CHOICES)
 
     class Meta(MetaPermissions):
         verbose_name = _('Баджет')
