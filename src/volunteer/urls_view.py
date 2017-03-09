37cbc2678cc52bdcc40834bdc7c30509536e08c7
Alex Uralov <a.uralov@wis.eu.com>
2017-03-09 20:01:22 +0300

    added auth via facebook

--- src/volunteer/urls_view.py
+++ src/volunteer/urls_view.py
@@ -79,7 +79,10 @@ def api_root(request, format=None):
         'admin:statistic:main': reverse('adm:statistic:main', **params),
 
         # Social auth
-        'social-auth:login': reverse('social:begin', **params, kwargs={'backend': 'vk-oauth2'}),
+        'social-auth:vk': reverse('social:begin', **params,
+                                  kwargs={'backend': 'vk-oauth2'}),
+        'social-auth:facebook': reverse('social:begin', **params,
+                                        kwargs={'backend': 'facebook'}),
 
         # Interviews
         'admin:interviews:interviewers': reverse(
