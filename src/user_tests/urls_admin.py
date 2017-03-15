ebdce8881f434e1be03b94780c1dbcf22e333166
Alex Uralov <a.uralov@wis.eu.com>
2017-03-15 22:05:34 +0300

    created AdminAverageTestScore view

--- src/user_tests/urls_admin.py
+++ src/user_tests/urls_admin.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
 from rest_framework import routers
 
-from user_tests.views import AdminUserTestViewSet
+from user_tests.views import AdminUserTestViewSet, AdminAverageTestScore
 
 router = routers.DefaultRouter()
+router.register('average-scores', AdminAverageTestScore,
+                base_name='average-score')
 router.register('', AdminUserTestViewSet, base_name='user-answer')
 
 
