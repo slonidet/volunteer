a6a8c3c2861ad6fb3355ab7fe861641d684b6bd6
Alex Uralov <a.uralov@wis.eu.com>
2017-03-16 17:57:57 +0300

    fixed finish_expired_test task

--- src/user_tests/tasks.py
+++ src/user_tests/tasks.py
@@ -7,6 +7,6 @@ from volunteer.app_celery import app
 @app.task(name='finish_expired_test')
 def finish_expired_test():
     for test in UserTest.objects.filter(finished_at=None):
-        if test.remaining <= 0:
+        if test.is_limited and test.remaining <= 0:
             test.finished_at = timezone.now()
             test.save()
