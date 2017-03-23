f307b185fd64977be83065fd126ffc811560e469
dmitry <dmitrysulin@gmail.com>
2017-03-23 15:42:13 +0300

    API for adding user to events

--- src/events/models.py
+++ src/events/models.py
@@ -4,18 +4,33 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.timezone import datetime
 
 from permissions.models import MetaPermissions
+from users.models import Profile, User
 
 
 class Event(models.Model):
     """
     Events model
     """
+    EVENT = 'event'
+    FORUM = 'forum'
+    EDUCATIONAL = 'educational'
+    TYPE_CHOICES = (
+        (EVENT, _('событие')),
+        (FORUM, _('форум')),
+        (EDUCATIONAL, _('образование')),)
+
     title = models.CharField(_('заголовок'), max_length=256)
+    type = models.CharField(_('тип мероприятия'), choices=TYPE_CHOICES,
+                            max_length=16)
     description = models.TextField(_('описание'))
     address = models.CharField(_('адрес'), max_length=512)
     start = models.DateTimeField(_('начало мероприятия'))
     end = models.DateTimeField(_('окончание мероприятия'))
     user_count = models.PositiveSmallIntegerField(_('количество участников'))
+    volunteer_count = models.PositiveSmallIntegerField(
+        _('количество волонтеров'))
+    users = models.ManyToManyField(
+        User, _('участники'), through='Participation')
     is_public = models.BooleanField(_('опубликовано'), default=True)
 
     class Meta(MetaPermissions):
@@ -29,3 +44,17 @@ class Event(models.Model):
     @property
     def is_actual(self):
         return self.start > datetime.now(tz=pytz.UTC)
+
+
+class Participation(models.Model):
+    """
+    Relational model between Event and Profile
+    """
+    PARTICIPANT = 'participant'
+    VOLUNTEER = 'volunteer'
+    STATUS_CHOICES = ((PARTICIPANT, 'participant'), (VOLUNTEER, 'volunteer'))
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    event = models.OneToOneField(Event, on_delete=models.CASCADE)
+    status = models.CharField(
+        _('статус в мероприятии'), max_length=16, choices=STATUS_CHOICES)
--- src/events/views.py
+++ src/events/views.py
@@ -1,10 +1,16 @@
 import pytz
 from django.utils.timezone import datetime
+from django.utils.translation import ugettext_lazy as _
+from django.db import IntegrityError
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework import exceptions
+from rest_framework.response import Response
 
 from events.filters import EventFilter
-from events.models import Event
+from events.models import Event, Participation
 from events.serializers import AdminEventSerializer, EventSerializer
+from users.models import User
 
 
 class AdminEventViewSet(viewsets.ModelViewSet):
@@ -13,8 +19,56 @@ class AdminEventViewSet(viewsets.ModelViewSet):
     filter_class = EventFilter
 
 
-class EventViewSet(viewsets.ReadOnlyModelViewSet):
+class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.filter(is_public=True,
                                     start__gt=datetime.now(tz=pytz.UTC))
     serializer_class = EventSerializer
     permission_classes = ()
+
+    @detail_route(methods=['post'], permission_classes=())
+    def participate(self, request, pk=None):
+        """
+        Add user in event
+        :param request:
+        :param pk:
+        :return:
+        """
+        user = self.request.user
+        event = Event.objects.get(id=pk)
+        approved_roles = [
+            'approved',
+            'tested',
+            'interviewed',
+            'prepared']
+        volunteer_roles = [
+            'main',
+            'reserved']
+        approved_users = User.objects.filter(role__in=approved_roles)
+        volunteers = User.objects.filter(role__in=volunteer_roles)
+
+        if user in approved_users or volunteers and event.type == 'event':
+            try:
+                Participation.objects.create(
+                    user=user, event=event, status='participant')
+                if user in approved_users:
+                    return Response(
+                        _('Вы записались на: {}, как участник').format(
+                            event.title))
+                if user in volunteers:
+                    return Response(
+                        _('Вы записались на: {}, как волонтер').format(
+                            event.title))
+            except IntegrityError:
+                error_msg = _('Вы уже записались на данное мероприятие')
+                raise exceptions.PermissionDenied(error_msg)
+
+        if user in volunteers or approved_users and event.type != 'event':
+            Participation.objects.create(
+                user=user, event=event, status='volunteer')
+            return Response(
+                _('Вы записались на: {}').format(event.title))
+
+        else:
+            error_msg = _('Чтобы принять участие в мероприятии, '
+                          'Ваш профиль должен быть подтвержден')
+            raise exceptions.PermissionDenied(error_msg)
