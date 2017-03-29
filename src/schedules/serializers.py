ab4638a1d1a6556fd94e0f10c04c4ec62ab3355c
Alex Uralov <a.uralov@wis.eu.com>
2017-03-29 11:39:18 +0300

    created AdminPlaceViewSet

--- src/schedules/serializers.py
+++ src/schedules/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from schedules.models import Shift, Period, Day
+from schedules.models import Shift, Period, Day, Place, Position, UserPosition
 
 
 class ShiftSerializer(serializers.ModelSerializer):
@@ -20,4 +20,28 @@ class PeriodSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Period
+        fields = '__all__'
+
+
+class UserPositionSerializer(serializers.ModelSerializer):
+    days = DaySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = UserPosition
+        fields = '__all__'
+
+
+class PositionSerializer(serializers.ModelSerializer):
+    user_positions = UserPositionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Position
+        fields = '__all__'
+
+
+class PlaceSerializer(serializers.ModelSerializer):
+    positions = PositionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Place
         fields = '__all__'
