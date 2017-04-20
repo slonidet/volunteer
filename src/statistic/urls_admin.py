1740233bc8ec36c4b7a84a8a76adff2502734427
dmitry <dmitrysulin@gmail.com>
2017-04-20 09:20:09 +0300

    Creating profile statistics functionality

--- src/statistic/urls_admin.py
+++ src/statistic/urls_admin.py
@@ -4,7 +4,7 @@ from statistic import views
 
 urlpatterns = [
     url(r'main/$', views.AdminStatistic.as_view(), name='main'),
-    url(r'equipment/$', views.EquipmentStatistic.as_view(), name='equipment')
+    url(r'equipment/$', views.EquipmentStatistic.as_view(), name='equipment'),
     url(r'profiles/gender-age$', views.ProfileGenderAgeStatView.as_view(),
         name='profiles_gender_age'),
     url(r'profiles/geo/$', views.ProfileGeoStatistic.as_view(), name='profiles_geo'),
