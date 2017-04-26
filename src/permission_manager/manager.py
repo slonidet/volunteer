902129e4accd3831d31b4d698b6835533aeacf5c
Alex Uralov <a.uralov@wis.eu.com>
2017-04-26 12:11:07 +0300

    refactoring

--- src/permission_manager/manager.py
+++ src/permission_manager/manager.py
@@ -3,14 +3,14 @@ from permission_manager.permissios_data import PERMISSIONS
 
 
 def set_permissions():
-    for group_name, descriptions in PERMISSIONS.items():
-        group, _ = Group.objects.get_or_create(name=group_name)
-        import pdb; pdb.set_trace()
-        for permission in generate_permissions(descriptions):
+    for group_name, permission_data in PERMISSIONS.items():
+        group, created = Group.objects.get_or_create(name=group_name)
+        for permission in generate_permissions(permission_data):
             group.permissions.add(permission)
 
-def generate_permissions(descriptions):
-    for description in descriptions:
+
+def generate_permissions(permission_data):
+    for description in permission_data:
         for action in description['actions']:
             code_name = '{}_{}'.format(action, description['model'])
             permission = Permission.objects.get_by_natural_key(
@@ -18,4 +18,5 @@ def generate_permissions(descriptions):
                 app_label=description['app_label'],
                 model=description['model'],
             )
+
             yield permission
