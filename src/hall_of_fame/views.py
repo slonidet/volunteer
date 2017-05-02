2f3b85ed67d0e9026d30690538bc142e14bd4930
dmitry <dmitrysulin@gmail.com>
2017-05-02 12:22:26 +0300

    temprorary

--- src/hall_of_fame/views.py
+++ src/hall_of_fame/views.py
@@ -11,6 +11,19 @@ class AdminHallOfFameViewSet(ModelViewSet):
     queryset = HallOfFame.objects.all().select_related('user__profile')
     serializer_class = AdminHallOfFameSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.data['user']
+        print(user)
+        if Story.profile.story:
+            print('yes')
+        else:
+            print('no')
+        # return super().create(request, *args, **kwargs)
+        # if user.profile.story:
+
 
 class AdminUsersHallOfFameViewSet(ModelViewSet):
     queryset = User.objects.select_related('profile')\
