fe0b9ec149c1084f9df298365b76bec52f3ccae5
Alex Uralov <a.uralov@wis.eu.com>
2017-05-03 23:28:15 +0300

    fixed events.signals.decrease_event_users_counter

--- src/events/signals.py
+++ src/events/signals.py
@@ -1,3 +1,4 @@
+from django.db import OperationalError
 from django.db.models import F
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
@@ -8,9 +9,13 @@ from events.models import Participation
 @receiver(pre_delete, sender=Participation)
 def decrease_event_users_counter(sender, instance, **kwargs):
     event = instance.event
-    if instance.status == Participation.STATUS_VOLUNTEER:
-        event.volunteers_count = F('volunteers_count') - 1
-        event.save()
-    if instance.status == Participation.STATUS_PARTICIPANT:
-        event.participants_count = F('participants_count') - 1
-        event.save()
+    participant_statuses = (
+        Participation.STATUS_VOLUNTEER, Participation.STATUS_PARTICIPANT
+    )
+
+    if instance.status in participant_statuses:
+        event.volunteers_count = F('{}s_count'.format(instance.status)) - 1
+        try:
+            event.save()
+        except OperationalError:
+            pass
