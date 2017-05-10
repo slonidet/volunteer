52b7193ec339e8e11b4c9a97f099e1100edebf0e
Alex Uralov <a.uralov@wis.eu.com>
2017-05-10 16:02:51 +0300

    VOL-401 refactored alert_users task

--- src/core/helpers.py
+++ src/core/helpers.py
@@ -25,9 +25,10 @@ def pluralization(number, string_of_words):
     list_of_words = string_of_words.split()
     a = number % 10
     b = number % 100
+
     if a == 1 and b != 11:
         return list_of_words[0]
-    elif a >= 2 and a <= 4 and (b < 10 or b >= 20):
+    elif (2 <= a <= 4) and (b < 10 or b >= 20):
         return list_of_words[1]
     else:
         return list_of_words[2]
--- src/events/tasks.py
+++ src/events/tasks.py
@@ -11,20 +11,24 @@ from volunteer.app_celery import app
 def alert_users():
     events = Event.objects.filter(is_public=True)
     time_lapses = (7, 3, 1)
-    day_forms = _('день дня дней')
 
     for event in events:
-        users = event.users
         if event.is_actual:
             remaining_time = event.start - timezone.now()
             remaining_days = remaining_time.days
             if remaining_days in time_lapses:
-                day_form = pluralization(remaining_days, day_forms)
-                for user in users:
-                    title = _('Напоминание о {}'.format(event.title))
-                    message = _('До мероприятия {0} осталось {1} {2}'
-                                .format(event.title, remaining_days, day_form))
-                    Notice.objects.create(
-                        title=title, message=message, user=user,
-                        type=Notice.TYPE_ALERT, is_confirmed=None,
-                    )
+                create_notices(event, remaining_days)
+
+
+def create_notices(event, remaining_days):
+    day_forms = _('день дня дней')
+    day_form = pluralization(remaining_days, day_forms)
+    title = _('Напоминание о {}'.format(event.title))
+    message = _('До мероприятия {0} осталось {1} {2}'
+                .format(event.title, remaining_days, day_form))
+
+    for user in event.users.all():
+        Notice.objects.create(
+            title=title, message=message, user=user,
+            type=Notice.TYPE_ALERT, is_confirmed=None,
+        )
