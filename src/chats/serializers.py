952ab0b01f6c87789115bd99813e287afc354c01
Dmitry Sazhnev <dmitrysulin@gmail.com>
2017-06-19 16:14:22 +0300

    Adding user object to message API

--- src/chats/serializers.py
+++ src/chats/serializers.py
@@ -1,24 +1,15 @@
-from rest_framework.serializers import ModelSerializer, ImageField
+from rest_framework.serializers import ModelSerializer
 
 from chats.models import Room, Message
-from core.serializers import HyperlinkedSorlImageField
 from users.serializers import UserSerializer
 
 
 class MessageSerializer(ModelSerializer):
     sender = UserSerializer(read_only=True)
-    photo = HyperlinkedSorlImageField(
-        '50x50', label='Фото', source='sender.profile_attachment.photo',
-        read_only=True
-    )
-    thumbnail = HyperlinkedSorlImageField(
-        '50x50', options={"crop": "center"},
-        source='photo', read_only=True
-    )
 
     class Meta:
         model = Message
-        fields = ('room', 'text', 'timestamp', 'sender', 'photo', 'thumbnail',)
+        fields = ('room', 'text', 'timestamp', 'sender', )
 
 
 class RoomSerializer(ModelSerializer):
